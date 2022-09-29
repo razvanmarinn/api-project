dd77d9310a6799229e8152701fc439fadb9902ba
realy1x <60939601+realy1x@users.noreply.github.com>
2022-09-29 18:37:25 +0300

    -

--- coinbase_api.py
+++ coinbase_api.py
@@ -7,7 +7,7 @@ import datetime
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
-  'limit':'3',
+  'limit':'3000',
   'convert':'USD'
 }
 headers = {
@@ -26,34 +26,26 @@ def api_pull_coinbase():
 
     coins = json['data']
     for x in coins:
-        # if x['symbol'] == 'BTC' or x['symbol'] == 'ETH':
-        symbol_coin =[]
-        price_usd = []
-        percent_change_24h = []
-        percent_change_7d = []
+        if x['symbol'] == 'DOGE' or x['symbol'] == 'ETH' or x['symbol'] == 'SHIB':
+          symbol_coin =[]
+          price_usd = []
+          percent_change_24h = []
+          percent_change_7d = []
 
-        symbol_coin.append(x['symbol']) 
-        price_usd.append(x['quote']['USD']['price'])
-        percent_change_24h.append(x['quote']['USD']['percent_change_24h'])
-        percent_change_7d.append(x['quote']['USD']['percent_change_7d'])
+          symbol_coin.append(x['symbol']) 
+          price_usd.append(x['quote']['USD']['price'])
+          percent_change_24h.append(x['quote']['USD']['percent_change_24h'])
+          percent_change_7d.append(x['quote']['USD']['percent_change_7d'])
 
-        dict = {'coin': symbol_coin, 'price': price_usd , 'percent change 24h': percent_change_24h, 'percent change 7d': percent_change_7d} 
-        df = pd.DataFrame(dict)   
-        df['date_time'] = pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:%S")
-        print(df)
-        #csv_writer.writerow([symbol_coin, price_usd, percent_change_24h, percent_change_7d])
-      
-        df.to_csv('file_name.csv', mode='a+', index=False, header=False)    
+          dict = {'coin': symbol_coin, 'price': price_usd , 'percent change 24h': percent_change_24h, 'percent change 7d': percent_change_7d} 
+          df = pd.DataFrame(dict)   
+          df['date_time'] = pd.to_datetime('now').strftime("%Y-%m-%d %H:%M:")
+          print(df)
+          #csv_writer.writerow([symbol_coin, price_usd, percent_change_24h, percent_change_7d])
+        
+          df.to_csv('csv/file_name.csv', mode='a+', index=False, header=False)    
        
       
 
-    
-def auto_repeat():
-    while 0 < 1:
-        api_pull_coinbase()
-        print("Api Pull done")
-        time.sleep(10)
-
-auto_repeat()
 
 
--- twitter.py
+++ twitter.py
@@ -1,13 +1,28 @@
 import tweepy
+import requests
+import pandas as pd
 import apikey
 
-auth = tweepy.OAuthHandler(
-   apikey.twitter_key, apikey.secret_key_tw
-)
-auth.set_access_token(apikey.acces_token_tw, apikey.acces_token_secret_tw)
+client = tweepy.Client( bearer_token=apikey.bearer_token,
+                        consumer_key=apikey.twitter_key, 
+                        consumer_secret=apikey.secret_key_tw, 
+                        access_token=apikey.acces_token_tw, 
+                        access_token_secret=apikey.acces_token_secret_tw, 
+                        return_type = requests.Response,
+                        wait_on_rate_limit=True)
 
-api = tweepy.API(auth)
+def twitter_api_pull():
+    query = 'bitcoin'
 
-public_tweets = api.home_timeline()
+# get max. 100 tweets 
+    tweets = client.get_recent_tweets_count(query = query , granularity = 'day')
+    reindex = ['start', 'end', 'tweet_count']
+    tweets_dict = tweets.json()
+    tweets_data = tweets_dict['data']
+    df_tw = pd.json_normalize(tweets_data)
+    df_tw=df_tw.reindex(columns=reindex)
+    df_tw['start'] = pd.to_datetime(df_tw['start'], errors='coerce').dt.strftime("%m/%d/%y")
+    df_tw['end'] = pd.to_datetime(df_tw['end'], errors='coerce').dt.strftime("%m/%d/%y")
+    df_tw.to_csv('csv/twitter_count_7d.csv' , mode = "a+" , index= False)
+    print(df_tw)
 
-print(public_tweets)
